e94104814c4023b2b6e236a29bad7a26409f8a23
kernel-enori <53615275+VaultEve@users.noreply.github.com>
2022-11-08 14:03:07 +0700

    Add files via upload

---
+++ Overwatch External/Overwatch External/Overwatch External.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <Windows.h>
+#include <thread>
+
+using namespace std;
+bool windowActived = false;
+bool foundOutput = false;
+
+void windowActive();
+void bunnyHop();
+
+int main()
+{
+    SetConsoleTitleA("Vaultality | External Bunny Hop");
+
+    thread windowActiveThread(windowActive);
+    thread bunnyHopThread(bunnyHop);
+
+    COORD coord = {};
+    coord.X = 200;
+    coord.Y = 150;
+
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hConsole, 13);
+    SetConsoleScreenBufferSize(hConsole, coord);
+
+
+    HWND consoleWindowHandle = GetConsoleWindow();
+    RECT r;
+    GetWindowRect(consoleWindowHandle, &r);
+    // SetWindowPos(consoleWindowHandle, HWND_TOPMOST, 0, 0, 0, 0, SWP_DRAWFRAME | SWP_NOMOVE | SWP_NOSIZE | SWP_SHOWWINDOW);
+    MoveWindow(consoleWindowHandle, r.left, r.top, 500, 250, TRUE);
+
+    cout << "Finding Game..." << endl;
+
+    while (true)
+    {
+        HWND hWnd = FindWindowA("TankWindowClass", nullptr);
+        if (hWnd)
+        {
+            if (!foundOutput)
+            {
+                system("cls");
+                cout << "Game Found!" << endl;
+                foundOutput = true;
+            }
+        }
+        Sleep(1000);
+    }
+
+    cin.get();
+
+    return 0;
+}
+
+void windowActive()
+{
+    while (true)
+    {
+        if (GetForegroundWindow() == FindWindowA("TankWindowClass", nullptr))
+        {
+            windowActived = true;
+        }
+        else
+        {
+            windowActived = false;
+        }
+        Sleep(100);
+    }
+}
+
+void bunnyHop()
+{
+    while (true)
+    {
+        if (GetAsyncKeyState(VK_SPACE) && windowActived)
+        {
+            PostMessage(GetForegroundWindow(), WM_KEYDOWN, 0x20, NULL);
+            PostMessage(GetForegroundWindow(), WM_KEYUP, 0x20, NULL);
+            Sleep(25);
+        }
+    }
+}
